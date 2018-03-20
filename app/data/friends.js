23223bb2db910db16fb7440913ec8d1f1a571b89
Your Name <jonathan.r.gilbert@gmail.com>
2018-03-20 00:10:22 -0700

    pushing up the API

---
+++ app/data/friends.js
@@ -0,0 +1,161 @@
+var friends = [{
+    name: "Billy",
+    photo: "https://www.pinterest.com/explore/billy-adventure-time/",
+    scores:
+        [
+            5,
+            1,
+            3,
+            5,
+            2,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+}, {
+    name: "Finn",
+    photo: "https://dreager1.files.wordpress.com/2013/06/finn_300.jpg",
+    scores:
+        [
+            2,
+            1,
+            4,
+            5,
+            3,
+            3,
+            3,
+            1,
+            1,
+            1
+        ]
+}, {
+    name: "Gandalf",
+    photo: "https://vignette.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754",
+    scores:
+        [
+            5,
+            2,
+            1,
+            1,
+            4,
+            1,
+            1,
+            1,
+            5,
+            1 
+        ]
+},{
+    name: "Garett",
+    photo: "https://www.giantbomb.com/forums/general-discussion-30/twelve-years-late-thief-ii-the-metal-age-1425760/",
+    scores:
+        [
+            5,
+            4,
+            2,
+            2,
+            4,
+            2,
+            2,
+            2,
+            5,
+            3
+        ]
+}, {
+    name: "Ice King",
+    photo: "https://nerdist.com/wp-content/uploads/2017/04/Adventure-Time-Featured-04072017.jpg",
+    scores:
+        [
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            2,
+            5,
+            4,
+            3
+        ]
+}, {
+    name: "The Litch",
+    photo: "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/9/92/Lich_Image.png/revision/latest?cb=20110523013725",
+    scores:
+        [
+            3,
+            1,
+            1,
+            5,
+            1,
+            1,
+            2,
+            5,
+            5,
+            5 
+        ]
+},{
+    name: "Magick Man",
+    photo: "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/ef/Magic_Man.png/revision/latest?cb=20100425011323",
+    scores:
+        [
+            1,
+            2,
+            1,
+            5,
+            1,
+            1,
+            1,
+            5,
+            5,
+            3
+        ]
+}, {
+    name: "Rincewind",
+    photo: "https://wiki.lspace.org/mediawiki/Rincewind",
+    scores:
+        [
+            4,
+            2,
+            4,
+            4,
+            3,
+            4,
+            2,
+            2,
+            4,
+            1 
+        ]
+}, {
+    name: "Saitama",
+    photo: "https://vignette.wikia.nocookie.net/onepunchman/images/0/07/Saitama_serious_profile.png/revision/latest?cb=20161002154538",
+    scores:
+        [
+            2,
+            1,
+            3,
+            3,
+            2,
+            1,
+            4,
+            1,
+            1,
+            1
+        ]
+},{
+    name: "Vaarsuvious",
+    photo: "https://img00.deviantart.net/9221/i/2010/063/c/a/oots___vaarsuvius_sketch_by_reevscythe.png",
+    scores:
+        [
+            5,
+            5,
+            1,
+            5,
+            2,
+            2,
+            1,
+            5,
+            4,
+            3
+        ]
+}];
